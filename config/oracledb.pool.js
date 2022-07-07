1bc2dc5a2337bca85f70956712397a9305de98eb
Paarthipan Prakash <prakashkovaidt@gmail.com>
2022-07-07 17:59:02 +0530

    oracle pool service added

--- config/oracledb.pool.js
+++ config/oracledb.pool.js
@@ -23,22 +23,23 @@ module.exports = {
                 user: process.env.NODE_ORACLEDB_USER,
                 password: process.env.NODE_ORACLEDB_PASSWORD,
                 connectString: process.env.NODE_ORACLEDB_CONNECTION_URI,
-                poolAlias: 'oracletest'
+                poolAlias: 'TRANDATA'
             });
         }
         if (ConnectMode == "TRANVIEW") {
+            console.log(ConnectMode);
             return await oracledb.createPool({
                 user: process.env.NODE_ORACLEDB_LIVEUSER,
                 password: process.env.NODE_ORACLEDB_LIVEPASSWORD,
                 connectString: process.env.NODE_ORACLEDB_LIVECONNECTION_URI,
-                poolAlias: 'oraclelive'
+                poolAlias: 'TRANVIEW'
             });
         } if (ConnectMode == "KSREQ") {
             return await oracledb.createPool({
                 user: process.env.NODE_ORACLEDB_REQUSER,
                 password: process.env.NODE_ORACLEDB_REQPASSWORD,
                 connectString: process.env.NODE_ORACLEDB_REQCONNECTION_URI,
-                poolAlias: 'oraclereq'
+                poolAlias: 'KSREQ'
             });
         }
     }
--- routes/oracle/api/index.js
+++ routes/oracle/api/index.js
@@ -2,18 +2,51 @@ var express = require('express');
 var router = express.Router();
 var { OracleTest } = require('../../../config/oracledb.config');
 var App = require('../../../controllers/oracle/approvalController');
+var Leave = require('../../../controllers/oracle/leaveController');
 
 var { select_query, closePoolAndExit } = require('../../../controllers/oracle/basicController');
 
 router.get('/', async function (req, res) {
     const opts = {};
-    const employee = await select_query('select * from employee_office where empcode  in (20169,11032)', 'oraclelive', opts);
-    const data = await App.checkLastRowError();
-    res.status(200).json({ result: employee, d: data.rows });
+
+    const employee = await select_query('select * from employee_office where empcode  in (20169,11032)', 'KSREQ', opts);
+    if (req.query.approvel == 'approve') {
+        const data1 = await App.checkLastRowError();
+        res.status(200).json({ result: employee.rows, data: data1.rows });
+    }
+    else if (req.query.leave == 'leave') {
+        const data1 = await Leave.checkLeaveError();
+        res.status(200).json({ result: employee.rows, data: data1.rows });
+    }
+    else {
+        res.status(404).json({ error: 'nos' });
+    }
+
 });
 router.post('/', async function (req, res) {
     //console.log(req.body);
     res.status(200).json({ result: 'posted data', postdata: req.body })
+});
+
+router.get('/monitor/:mode', async function (req, res) {
+    let query_result;
+    switch (req.params['mode']) {
+        case 'approvefinal':
+            query_result = await App.checkLastRowError();
+            res.status(200).json({ result: query_result.rows });
+            break;
+        case 'leave':
+            query_result = await Leave.checkLeaveError();
+            res.status(200).json({ result: query_result.rows });
+            break;
+        case 'approvedisplay':
+            query_result = await App.checkApprovalDisplayissue();
+            res.status(200).json({ result: query_result, main: JSON.parse(query_result.app[0]) });
+            break;
+        default:
+            res.status(200).json({ result: 'not found' });
+            break;
+    }
 });
 
 process
